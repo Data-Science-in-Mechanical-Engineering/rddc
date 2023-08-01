28b966c493ee6fa7bec21fd9ab2520afe3c6db41
Dmitrii Likhachev <dmitrii.likhachev@rwth-aachen.de>
2023-08-01 11:48:18 +0200

    New setting sim2real and exp trajectories

--- rddc/run/settings/simulation_like_experiment.py
+++ rddc/run/settings/simulation_like_experiment.py
@@ -3,7 +3,7 @@ import numpy as np
 def get_settings():
     name = 'simulation'
     suffix = 'like_experiment'
-    seed = 550
+    seed = 20
     eps = 1e-8
     controllability_tol = 1e-3
     state_idx = [0,1,3,4,6,7]
@@ -20,45 +20,73 @@ def get_settings():
     check_willems = False
     algorithm = 'robust_stabilization_scenario_slemma'
     # algorithm = 'robust_lqr_scenario_slemma'
+    # algorithm = 'robust_h2_scenario_slemma'
     output_verbosity = 0
 
     # Extra weight distribution
     extra_loads = list() # do not touch this one, only adjust extra_loads_synth or extra_loads_test
     extra_loads_synth = list() # leave empty ("list()") if you want to pick them randomly
     # extra_loads_synth = [
-    #     {'mass': 0.001 ,'position':np.array([ 0.003,  0.002, -0.001]), 'form':'ball', 'size':[0.0]},
+        # {'mass': 0.000 ,'position':np.array([ 0.000,  0.000, -0.000]), 'form':'ball', 'size':[0.0]},
     #     {'mass': 0.0013,'position':np.array([-0.002,  0.001,  0.001]), 'form':'ball', 'size':[0.0]},
     #     {'mass': 0.0013,'position':np.array([ 0.000, -0.004,  0.002]), 'form':'ball', 'size':[0.0]},
     #     {'mass': 0.0007,'position':np.array([ 0.006, -0.001,  0.000]), 'form':'ball', 'size':[0.0]},
     #     {'mass': 0.0018,'position':np.array([-0.001, -0.001, -0.001]), 'form':'ball', 'size':[0.0]},
     # ]
     extra_loads_test = list() # leave empty ("list()") if you want to pick them randomly
-    mass_range = [0.007, 0.012]
-    pos_size = [0.01, 0.01, 0.002]
+    mass_range = [0.007, 0.015]
+    pos_size = [0.015, 0.015, 0.01]
     # displacement_planar = 0.01
     # displacement_vert = 0.0
     filenames = [
-        'hover_000000_10Hz_1.0',
-        'hover_000003_10Hz_1.0',
-        'hover_001002_10Hz_1.0',
-        'hover_010001_10Hz_1.0',
-        'hover_010201_10Hz_1.0',
-        'hover_011002_10Hz_1.0',
-        'hover_100000_10Hz_1.0',
-        'hover_100012_10Hz_1.0',
-        'hover_103010_10Hz_1.0',
-        'hover_110002_10Hz_1.0',
-        'hover_110010_10Hz_1.0',
-        'hover_120001_10Hz_1.0',
-        'hover_120002_10Hz_1.0',
-        'hover_221000_10Hz_1.0',
-        'hover_300002_10Hz_1.0',
+        # 'hover_000000_10Hz_1.0',
+        # 'hover_000003_10Hz_1.0',
+        # 'hover_001002_10Hz_1.0',
+        # 'hover_010001_10Hz_1.0',
+        # 'hover_010201_10Hz_1.0',
+        # 'hover_011002_10Hz_1.0',
+        # 'hover_100000_10Hz_1.0',
+        # 'hover_100012_10Hz_1.0',
+        # 'hover_103010_10Hz_1.0',
+        # 'hover_110002_10Hz_1.0',
+        # 'hover_110010_10Hz_1.0',
+        # 'hover_120001_10Hz_1.0',
+        # 'hover_120002_10Hz_1.0',
+        # 'hover_221000_10Hz_1.0',
+        # 'hover_300002_10Hz_1.0',
+        ##### Second session #####
+        'hover_000000_10Hz_1.0_',
+        # 'hover_000001_10Hz_0.9',
+        # 'hover_000020_10Hz_0.9',
+        # 'hover_000400_10Hz_1.0',
+        # 'hover_000512_10Hz_0.9_fewPoints',
+        # # 'hover_000512_10Hz_1.0_fewPoints',
+        # 'hover_012000_10Hz_0.9',
+        # # 'hover_020031_10Hz_1.0_fewPoints',
+        # 'hover_100003_10Hz_1.0',
+        # 'hover_101002_10Hz_0.9',
+        # # 'hover_101003_10Hz_0.9_fewPoints',
+        # 'hover_120000_10Hz_1.0',
+        # 'hover_120002_10Hz_0.9',
+        # 'hover_201011_10Hz_1.0',
+        # 'hover_220001_10Hz_1.0',
+        # # 'hover_220011_10Hz_1.0_fewPoints',
+        # 'hover_221000_10Hz_0.9',
+        # 'hover_300002_10Hz_0.9_fewPoints',
+        ### 50Hz #####################
+        # 'hover_000000_50Hz_1.5',
+        # 'hover_000000_50Hz_2.5_vicon300Hz1sample',
+        # 'hover_020000_50Hz_1.1',
+        ### 20 Hz ####################
+        # 'hover_000000_20Hz_0.5',
+        ### 5 Hz #####################
+        # 'hover_000000_5Hz_1.0',
     ]
 
-    N_synth = 15
+    N_synth = 10
     N_test = 100
     start = 0                              # time step to start sampling the trajectory with
-    T = 500                                  # number of samples per trajectory for controller synthesis
+    T = 1000                               # number of samples per trajectory for controller synthesis
     T_test = 60                            # number of samples per trajectory for performance evaluation
 
     # noise
@@ -76,12 +104,12 @@ def get_settings():
     trainSettings = {
         'num_drones':N_synth,
         'sfb':None,
-        'sfb_freq_hz':10,
+        'sfb_freq_hz':20,
         'num_samples':T,
-        'ctrl_noise':1.0,
-        'proc_noise':0.0005,
+        'ctrl_noise':2.0,
+        'proc_noise':0.005,
         'traj':'hover',
-        'part_pid_off':True,
+        'part_pid_off':False,
         'traj_filename':None,
         'plot':False,
         'cut_traj':True,
@@ -90,7 +118,7 @@ def get_settings():
         'gui':True,
         'pid_type':'mellinger',
         'control_freq_hz':500,
-        'simulated_delay_ms':8
+        'simulated_delay_ms':10
     }
     testSettings = {
         'num_drones':N_test,
