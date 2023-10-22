f2dd541f6a90e2714b410faa8d0ccdfc9965f98e
Dmitrii Likhachev <dmitrii.likhachev@rwth-aachen.de>
2023-10-22 14:22:10 +0200

    Remove obsolete and not implemented testing functions

--- rddc/tools/testing.py
+++ rddc/tools/testing.py
@@ -17,13 +17,6 @@ def test_stability(systems, K, eps=1e-6):
 
     return N_stable
 
-def test_stability_empiric(systems, K, simInfo, noiseInfo):
-    pass
-
-def test_performance(systems, K, distribution):
-    #based on lqr_cost
-    pass
-
 def test_performance_empiric(systems, K, T, settings, N_traj_per_sys, stab_eps=1e-6):
     #based on lqr_cost_empiric
     costs = list()
@@ -38,40 +31,6 @@ def test_performance_empiric(systems, K, T, settings, N_traj_per_sys, stab_eps=1
         )
         costs.append(cost)
     return np.array(costs)
-
-def test_performance_trajectories(trajectories, distribution):
-    #based on lqr_cost_trajectories
-    pass
-
-
-def lqr_cost(system, K, metric, stab_eps):
-    A = system[0]
-    B = system[1]
-
-    A_cl = A + B @ K
-    if not control_utils.check_stability(A_cl, eps=stab_eps):
-        return np.Inf
-
-    Q_cl = metric['Q'] + K.T @ metric['R'] @ K
-
-    X_Q = pc.SymmetricVariable('X_Q', shape=A.shape)
-
-    F = pc.Problem()
-
-    F.set_objective('min', pc.trace(X_Q))
-
-    F.add_constraint(A_cl.T * X_Q * A_cl - X_Q + Q_cl == 0)
-    F.add_constraint(X_Q >> 0)
-
-    F.solve(verbosity=0, primals=None)
-
-    # Unstable, so expected cost is infinite
-    if F.status != 'optimal':
-        return np.Inf
-
-    X_Q = np.atleast_2d(X_Q.value)
-
-    return np.trace(n * self.V @ X_Q)
     # TODO: Idk what is n and self.V
 
 def lqr_cost_empiric(system, T, K, settings, stab_eps, N_traj_per_sys=100):
